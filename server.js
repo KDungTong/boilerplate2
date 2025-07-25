96b607f8b210368312368cbdf0f3b599b9acd7a8
Dung Tong <tongkhanhdung3008@gmail.com>
2025-07-25 17:03:59 +0700

    fix logout

--- server.js
+++ server.js
@@ -8,8 +8,9 @@ const LocalStrategy = require('passport-local');
 const app = express();
 const session = require('express-session');
 const passport = require('passport');
-const { ObjectID } = require('mongodb');
-// add this CORS header
+const { ObjectId } = require('mongodb');
+
+// CORS header (có thể bỏ nếu không cần)
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
@@ -19,7 +20,7 @@ app.use((req, res, next) => {
 
 app.set('view engine', 'pug');
 app.set('views', './views/pug');
-fccTesting(app); // For fCC testing purposes
+fccTesting(app);
 
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
@@ -31,59 +32,102 @@ app.use(session({
   saveUninitialized: true,
   cookie: { secure: false }
 }));
+
 app.use(passport.initialize());
 app.use(passport.session());
 
-  app.route('/login').post(passport.authenticate('local', { failureRedirect: '/' }), (req, res) => {
-    res.redirect('/profile');
-     // Store username in session
-  })
-
-  app.route('/profile').get(ensureAuthenticated, (req,res) => {
-    res.render('profile');
-    {username: req.user.username};
-  })
-    // Logged a user out
-  app.route('/logout')
-    .get((req, res) => {
-      req.logout();
-      res.redirect('/');
-    });
-    
-
-  //Create a new midddleware
-  function ensureAuthenticated(req, res, next) {
+// Middleware bảo vệ route
+function ensureAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
   }
   res.redirect('/');
-};
+}
 
-app
- .route('/profile')
- .get(ensureAuthenticated, (req,res) => {
-    res.render('profile');
- });
- 
 myDB(async client => {
   const myDataBase = await client.db('database').collection('users');
 
-  app.route('/').get((req, res) => {
-    res.render('index', {
-      title: 'Connected to Database',
-      message: 'Please log in',
-      showLogin: true
+  // Passport strategy
+  passport.use(new LocalStrategy((username, password, done) => {
+    myDataBase.findOne({ username: username }, (err, user) => {
+      console.log(`User ${username} attempted to log in.`);
+      if (err) return done(err);
+      if (!user || user.password !== password) return done(null, false);
+      return done(null, user);
     });
-  });
+  }));
 
   passport.serializeUser((user, done) => {
     done(null, user._id);
   });
-  
+
   passport.deserializeUser((id, done) => {
-    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-      done(null, doc);
+    myDataBase.findOne({ _id: new ObjectId(id) }, (err, user) => {
+      done(null, user);
     });
+  });
+
+  // Home
+  app.route('/').get((req, res) => {
+    res.render('index', {
+      title: 'Connected to Database',
+      message: 'Please log in',
+      showLogin: true,
+      showRegistration: true,
+      showSocialAuth: true
+    });
+  });
+
+  // Register
+  app.route('/register')
+    .post((req, res, next) => {
+      myDataBase.findOne({ username: req.body.username }, (err, user) => {
+        if (err) return next(err);
+        if (user) return res.redirect('/');
+        myDataBase.insertOne({
+          username: req.body.username,
+          password: req.body.password
+        }, (err, result) => {
+          if (err) return res.redirect('/');
+          next(null, result.ops[0]);
+        });
+      });
+    },
+      passport.authenticate('local', { failureRedirect: '/' }),
+      (req, res) => {
+        res.redirect('/profile');
+      }
+    );
+
+  // Login
+  app.route('/login')
+    .post(passport.authenticate('local', { failureRedirect: '/' }),
+      (req, res) => {
+        res.redirect('/profile');
+      });
+
+  // Profile
+  app.route('/profile')
+    .get(ensureAuthenticated, (req, res) => {
+      res.render('profile', { username: req.user.username });
+    });
+
+  // Logout
+app.route('/logout')
+  .get((req, res, next) => {
+    req.logout(function(err) {
+      if (err) return next(err);
+      req.session.destroy(function(err) {
+        if (err) return next(err);
+        res.clearCookie('connect.sid'); // Quan trọng để xóa cookie
+        res.redirect('/');
+      });
+    });
+  });
+
+  // 404
+  app.use((req, res) => {
+    res.status(404).type('text').send('Not Found');
   });
 
 }).catch(e => {
@@ -91,25 +135,6 @@ myDB(async client => {
     res.render('index', { title: e, message: 'Unable to connect to database' });
   });
 });
-
-
-passport.use(new LocalStrategy((username, password, done) => {
-  myDataBase.findOne({ username: username }, (err, user) => {
-    console.log(`User ${username} attempted to log in.`);
-    if (err) return done(err);
-    if (!user) return done(null, false);
-    if (password !== user.password) return done(null, false);
-    return done(null, user);
-  });
-}));
-
-function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next();
-  }
-  res.redirect('/');
-}; 
-
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
